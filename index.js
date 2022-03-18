6210f40c2aa22007abee05ebff211cf711614723
Toby <tobyxs91@gmail.com>
2022-03-18 12:20:25 +0100

    hmm

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 let playerscore = 0
 let computerscore = 0
 
+let rockVar = document.querySelector(".rockbutton")
+let paperVar = document.querySelector(".paperbutton")
+let scissorsVar = document.querySelector(".scissorsbutton")
+
+rockVar.addEventListener("click", function() {
+  let result = (playRound(computerPlay(), "rock"))
+  scoreCheck(result)
+})
+
+paperVar.addEventListener("click", function() {
+  let result = (playRound(computerPlay(), "paper"))
+  scoreCheck(result)
+})
+
+scissorsVar.addEventListener("click", function() {
+  let result = (playRound(computerPlay(), "scissors"))
+  scoreCheck(result)
+})
+
+// score check uses the result from playround to add the scores, display the score,
+// and check if the game is over
+
+
+function scoreCheck(result) {
+  if (result == "You win") {
+    playerscore++
+    document.querySelector("div").style.color = "green"
+    document.querySelector("div").textContent = "You win!"
+  } else if (result == "The Computer Wins") {
+    computerscore++
+    document.querySelector("div").style.color = "red"
+    document.querySelector("div").textContent = "You lose!"
+  } else if (result == "It's a tie") {
+    document.querySelector("div").style.color = "blue"
+    document.querySelector("div").textContent = "It's a tie"
+  } else {
+    "error 832"
+  }
+
+  document.querySelector(".scoreDiv").textContent = ("player score: " + playerscore + ". computer score: " + computerscore + ".")
+
+  if ((playerscore == 5) || (computerscore == 5)) {
+    if (playerscore > computerscore) {
+      alert("Congratulations you won!")
+    } else {
+      alert("Better luck next time!")
+    }
+    alert("please reload")
+  }
+}
+
+
+// get computer rock/paper/scissos
 function computerPlay() {
   var computerReturn = Math.floor(3 * Math.random());
   if (computerReturn === 0) {
@@ -11,10 +64,10 @@ function computerPlay() {
     return "scissors"
   } else {
     alert("error 529")
-    console.log("error 592")
   }
 }
 
+// plays round with comp selection and player selection
 function playRound(computerSelection, playerSelection) {
   if (computerSelection == playerSelection) {
     return "It's a tie"
@@ -36,35 +89,3 @@ function playRound(computerSelection, playerSelection) {
     reload()
   }
 }
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let computerSelection = computerPlay();
-    let playerSelection = prompt("Choose your Character! Either rock, paper or scissors.").toLowerCase();
-    let result = playRound(computerSelection, playerSelection);
-    if (result == "You win") {
-      playerscore++
-    } else if (result == "The Computer Wins") {
-      computerscore++
-    } else if (result == "It's a tie") {
-      console.log("tie")
-    } else {
-      "error 832"
-    }
-
-    alert("You chose " + playerSelection + ". The computer chose " + computerSelection + ". " + result + ".\nAfter round " + (i + 1) + ": player " + playerscore + ", computer " + computerscore + " .")
-
-  }
-}
-
-alert("Welcome to rock, paper scissors. This will be a 5 round game. Good luck!")
-
-game()
-
-if (playerscore > computerscore) {
-  alert("Congratulations you won!")
-} else {
-  alert("Better luck next time!")
-}
-
-alert("player selection is " + playerSelection)
